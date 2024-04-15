76d53f91b8002cc5768b392642d5bedd8ee19921
Ali Rıza Darcan <99339675+alidarcan@users.noreply.github.com>
2024-04-15 02:09:06 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Table from "./components/table";
+
+function App() {
+  return (
+    <main>
+      <div>
+        <h1>Sudoku Solver</h1>
+        <p>Hello There</p>
+        <Table/>
+      </div>
+    </main>
+  );
+}
+
+export default App;
